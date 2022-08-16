68755dcb919a1b9f5fb988268be3499fc551a478
rayjackson13 <rayjackson@live.ru>
2022-08-16 13:51:06 +0300

    Further translations.

--- src/components/EditPage/NotFound/index.svelte
+++ src/components/EditPage/NotFound/index.svelte
@@ -1,13 +1,17 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+</script>
+
 <div class="sm:px-5 flex flex-wrap">
   <div class="w-full xl:w-5/12 sm:px-5 pt-24 text-center">
     <h1 class="text-xl mb-12">
-      Sorry, this budget sheet doesn't exist or you don't have permission to access it.
+      {$_('edit.not-found.title')}
     </h1>
     <a
       href="/dashboard"
       class="rounded-3xl shadow text-lg bg-blue-500 text-white px-16 py-4 hover:opacity-50 transition-opacity"
     >
-      View my sheets
+    {$_('edit.not-found.cta')}
     </a>
   </div>
 </div>
